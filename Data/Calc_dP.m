65a4bab3f82e80de97b320a51db9a2aa60e46911
Andrew <bossman96@outlook.com>
2015-11-18 16:44:26 -0600

    Adjusting generation of permittivity and conductivity

--- Data/Calc_dP.m
+++
@@ -1,3 +0,0 @@
-function dielectric_Permittivity = Calc_dP(d,A,Z)
-dielectric_Permittivity = @(omega)(d)./(omega.*A.*imag(Z));
-end
--- Data/Conductivity_Permitivity.m
+++ Data/Conductivity_Permitivity.m
@@ -1,44 +1,47 @@
-d = 0.01;                                                 %Plate distance
-A = 25.4*(3/4);                                           %Inch to mm
-omega = freq.*2.*pi;                                      %Omega conversion
+omega = freq.*2.*pi;
+
 Zin = (z11_cells - z12_cells)-((z11_air - z12_air)+(z11_PBS-z12_PBS));
 Zp = (z12_cells - (z12_air + z12_PBS));
 Zout = (z22_cells - z12_cells)-((z22_air - z12_air)+(z22_PBS - z12_PBS));
 
-dielectric_Permittivity_Zin = Calc_dP(d,A,Zin);
-dielectric_Permittivity_Zp = Calc_dP(d,A,Zp);
-dielectric_Permittivity_Zout = Calc_dP(d,A,Zout);
+dP_Zin = Calc_Permittivity(d,A,Zin,omega);
+dP_Zp = Calc_Permittivity(d,A,Zp,omega);
+dP_Zout = Calc_Permittivity(d,A,Zout,omega);
+
+c_Zin = Calc_Conductivity(d,A,Zin);
+c_Zp = Calc_Conductivity(d,A,Zp);
+c_Zout = Calc_Conductivity(d,A,Zout);
 
 figure
-subplot(2,2,1)
-scatter(freq,dielectric_Permittivity_Zin(omega));
+subplot(3,1,1)
+scatter(freq,dP_Zin);
 title('Dielectric Permittivity \epsilon_{in}');
 xlabel('Frequency(Hz)');
 ylabel('\epsilon');
-subplot(2,2,2)
-scatter(freq,dielectric_Permittivity_Zp(omega));
+subplot(3,1,2)
+scatter(freq,dP_Zp);
 title('Dielectric Permittivity \epsilon_{p}');
 xlabel('Frequency(Hz)');
 ylabel('\epsilon');
-subplot(2,2,3)
-scatter(freq,dielectric_Permittivity_Zout(omega));
+subplot(3,1,3)
+scatter(freq,dP_Zout);
 title('Dielectric Permittivity \epsilon_{out}');
 xlabel('Frequency(Hz)');
 ylabel('\epsilon');
 
 figure
-subplot(2,2,1)
-scatter(freq,Calc_Conductivity(d,A,Zin));
+subplot(3,1,1)
+scatter(freq,c_Zin);
 title('Conductivity \sigma_{in}');
 xlabel('Frequency(Hz)');
 ylabel('\sigma');
-subplot(2,2,2)
-scatter(freq,Calc_Conductivity(d,A,Zp));
+subplot(3,1,2)
+scatter(freq,c_Zp);
 title('Conductivity \sigma_{p}');
 xlabel('Frequency(Hz)');
 ylabel('\sigma');
-subplot(2,2,3)
-scatter(freq,Calc_Conductivity(d,A,Zout));
+subplot(3,1,3)
+scatter(freq,c_Zout);
 title('Conductivity \sigma_{out}');
 xlabel('Frequency(Hz)');
 ylabel('\sigma');
